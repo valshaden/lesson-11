c713c9e5f2a1010d85102c877727e256bff7afc5
valshaden <valshaden@yandex.ru>
2025-08-14 18:31:25 +0300

    Поверка

--- my_module.py
+++ my_module.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 def study_entry():
     window = tk.Toplevel()
@@ -6,11 +7,26 @@ def study_entry():
     window.geometry("300x200")
     
     
+    tk.Label(window, text="Имя:").pack(pady=5)
+    entry1 = tk.Entry(window)
+    entry1.pack(pady=5)
+    
+    tk.Label(window, text="Пароль:").pack(pady=5)
+    entry2 = tk.Entry(window, show="*")
+    entry2.pack(pady=5)
+    
+    #def show_values():
+    #    name = entry1.get()
+    #    password = entry2.get()
+    #    print("Имя:", name)
+    #    print("Пароль:", password)
+    
+    def show_values():
+        messagebox.showinfo("Значения полей ввода", f"Имя: {entry1.get()}\nПароль: {entry2.get()}")
+    #    name = entry1.get()
+    #    password = entry2.get()
+    #    print("Имя:", name)
+    #    print("Пароль:", password)
 
-
-#def button_click():
-#№    print("Кнопка нажата!")print(__name__)
-#№if __name__ == "__main__":
-#№    print(2, "Самостоятельный запуск")
-#№else :
-#№    print(2, "Импорт модуля")
+    tk.Button(window, text="Показать", command=show_values).pack(pady=10)
+                                       
