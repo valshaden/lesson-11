16b8b40db6ebe766b97233c27c547a256d31423e
valshaden <valshaden@yandex.ru>
2025-08-14 18:08:39 +0300

    + Кнопка

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Изучение элементов GUI")
     root.geometry("600x400+700+300")
+    tk.Button(root, text="Изучение полей ввода").pack()
+    
+    
     
     root.mainloop()    
     
