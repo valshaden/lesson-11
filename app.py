33f7d213e91761ba6d87c9f135da5a28daf8f3d1
valshaden <valshaden@yandex.ru>
2025-08-14 17:42:45 +0300

    Начал работать с модулями

---
+++ app.py
@@ -0,0 +1,10 @@
+import my_module
+
+print("Hello World")
+
+#exit(100)
+
+x = my_module.summa(10, 20)
+print(x)
+print(my_module.pi)
+
---
+++ my_module.py
@@ -0,0 +1,11 @@
+pi = 3.14
+
+def summa(a:int, b:int) -> int:
+    '''
+    This function takes two numbers as input and returns their sum.
+    '''
+    return a + b
+
+print("Привет из модуля")
+
+#exit(200)
