165781b76739c34d3fef7a61469e7482da175ed0
valshaden <valshaden@yandex.ru>
2025-08-18 12:36:15 +0300

    arc 2

--- arkanoid.py
+++ arkanoid.py
@@ -5,6 +5,6 @@ root.title("Арканоид")
 canvas = tk.Canvas(root, width=600, height=400, bg="black")
 canvas.pack()
 
-
+paddle = canvas.create_rectangle(250, 350, 350, 370, fill="white")
 
 root.mainloop()
