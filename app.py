ee69476749d6f048544fa330dd15cba49a04922f
valshaden <valshaden@yandex.ru>
2025-08-18 11:34:14 +0300

    + in git

--- app.py
+++ app.py
@@ -6,18 +6,14 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("Изучение элементов GUI")
     root.geometry("600x400+700+300")
-    #tk.Button(root, text="Изучение полей ввода").pack()
     
     tk.Button(root,
               text="Изучение полей ввода", 
               command=study_entry).pack(pady=20)
+    
     tk.Button(root, 
               text="Изучение радиокнопок", 
               command=study_radio).pack(pady=20)
     
-    
-    
+        
     root.mainloop()
-
-
-    
--- study_radiobutton.py
+++ study_radiobutton.py
@@ -1,22 +1,21 @@
 import tkinter as tk
 from tkinter import messagebox
 
+
 def study_radio():
-
-    window = tk.Tk()
-    window.title("Радио кнопки")
+    """Изучение радиокнопок"""
+    window = tk.Toplevel()
+    window.title("Радиокнопки")
     window.geometry("300x200")
-
+    
     var = tk.StringVar(value="python")
-
-    tk.Label(text="Выберите язык программирования:").pack()
-    tk.Radiobutton(text="Python", variable=var, value="Python").pack()
-    tk.Radiobutton(text="Java", variable=var, value="Java").pack()
-    tk.Radiobutton(text="C++", variable=var, value="C++").pack()
-
-    def show_value():
-        messagebox.showinfo("Результат", f"Выбран язык: {var.get()}")
-
-    tk.Button(window, text="Показать", command=show_value).pack(pady=10)
-
-#window.mainloop()
+    
+    tk.Label(window, text="Выберите язык:").pack(pady=5)
+    tk.Radiobutton(window, text="Python", variable=var, value="python").pack()
+    tk.Radiobutton(window, text="Java", variable=var, value="java").pack()
+    tk.Radiobutton(window, text="C++", variable=var, value="cpp").pack()
+    
+    def show_choice():
+        messagebox.showinfo("Результат", f"Выбран: {var.get()}")
+    
+    tk.Button(window, text="Показать", command=show_choice).pack(pady=10)
